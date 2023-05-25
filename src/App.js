6a4dbbfe6a39288e5a4ab575c99a3316ac95f04d
NadiaPia <206i417@gmail.com>
2023-05-25 01:01:43 -0700

    Add useEffect to the App.js

--- src/App.js
+++ src/App.js
@@ -10,35 +10,36 @@ import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import Registration from "./pages/Registration";
 import Login from "./pages/Login";
+//import { useNavigate } from "react-router-dom";
+
 
 
 
 
 function App() {
 
+  //const navigate = useNavigate();
+
   axios.defaults.withCredentials = true;//it sende cookies info to the server in every axios requests and it sets access-tokennn in the Application tab
-
-
-  /*useEffect(() => {
-    axios.get("http://localhost:3001/auth/auth", { withCredentials: true }).then((response) => {
-      console.log("----------")
-    })
-
-
-  }, [])*/
-
   
-  useEffect(() => {
-    console.log("before axios.det login")
+  const [url, setUrl] = useState('');
+  const [authState, setAuthState] = useState({ username: "", id: 0, status: false });
+  
+  useEffect(() => {    
     axios.get("http://localhost:3001/auth/login").then((response) => {
-      console.log(response)
+      //console.log("response.data", response.data); //{username: 'pavel', id: 54, iat: 1684998132}
+      setAuthState({username: response.data.username, id: response.data.id, status: true});
 
-    })
+    }).catch((err) => {
+      //console.log(err)
+      setAuthState({ ...authState, status: false });
+      //navigate("/");
+
+    }) 
   }, [])
   
 
-  const [url, setUrl] = useState('');
-  const [authState, setAuthState] = useState({ username: "", id: 0, status: false });
+  
 
 
   return (
